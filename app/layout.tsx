86e36ffe36559ba9abd0d785ef28f53dfb9faeca
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2023-08-28 05:37:44 +0600

    added framer motion to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Saleh Shakib | Personal Portfolio",
   description:
-    "Hey, I am a fresh graduate from Ahsanullah University of Science and Technlogy with a Bachelor of Science degree on Computer Science and Engineering ",
+    "Hey, I am a fresh graduate from Ahsanullah University of Science and Technlogy with a Bachelor of Science degree on Computer Science and Engineering.",
 };
 
 export default function RootLayout({
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:[-28rem] xl:[-15rem]"></div>
+
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
